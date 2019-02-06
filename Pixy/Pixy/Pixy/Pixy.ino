296dff55823f506cec71c6ae3ae287c5bd1fd9da
Unknown <20kweeda@fpsct.org>
2019-02-05 19:19:55 -0500

    finally figured out arduino as slave to roborio code

--- Pixy/Pixy/Pixy/Pixy.ino
+++ Pixy/Pixy/Pixy/Pixy.ino
@@ -1,23 +1,21 @@
-
 #include <SPI.h>
 #include <Pixy.h>
 #include <Wire.h>
 
 Pixy pixy;
-String b = "ben";
+byte b = 11;  
 
 void setup() 
-{  
+{
   Wire.begin(8);
   Wire.onRequest(requestEvent);
   
   pixy.init();
 }
 
-void requestEvent()//might fix, its worth a try 
+void requestEvent()//might fix, its a tried 
 {
   Wire.write(178);
-  //Serial.print("yay");
 }
 
 void loop() 
@@ -27,25 +25,28 @@ void loop()
   uint16_t blocks;
   
   blocks = pixy.getBlocks();
-  //Serial.print("Cup\n");
+  Serial.print("Cup\n");
   
   if (blocks)
   {
     //r
     i++;
-    //Serial.print("Blue\n");
+    Serial.print("Blue\n");
 
     //This is in place so that the Arduino will take data when the Pixy is at
     //50 frames per second to prevent the Pixy from overloading
     if (i%50==0)
     {
+
       //this is where we want to write
+
     }
   }
   else
+    
   {
     // return -1
-    //Serial.print("The\n");
+    Serial.print("The\n");
   }
   
 }
