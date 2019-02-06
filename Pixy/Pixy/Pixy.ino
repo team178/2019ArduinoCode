7a04017230e1cf7fc72070f7c42d7ffb3420453c
Unknown <20kweeda@fpsct.org>
2019-02-05 19:15:47 -0500

    got arduino as slave to roborio comm working
    need to use "on request"

--- Pixy/Pixy/Pixy.ino
+++ Pixy/Pixy/Pixy.ino
@@ -4,24 +4,20 @@
 #include <Wire.h>
 
 Pixy pixy;
-
+String b = "ben";
 
 void setup() 
-{
-  byte b = 11;  Serial.begin(9600);
-  Serial.print("Starting...\n");
-  Wire.begin();
-  Wire.beginTransmission(8);
+{  
+  Wire.begin(8);
   Wire.onRequest(requestEvent);
-  //Wire.write(b);//for testing
-  Wire.endTransmission(8);
   
   pixy.init();
 }
 
-void requestEvent()//might fix, its a tried 
+void requestEvent()//might fix, its worth a try 
 {
-  Wire.write(b);
+  Wire.write(178);
+  //Serial.print("yay");
 }
 
 void loop() 
@@ -31,28 +27,25 @@ void loop()
   uint16_t blocks;
   
   blocks = pixy.getBlocks();
-  Serial.print("Cup\n");
+  //Serial.print("Cup\n");
   
   if (blocks)
   {
     //r
     i++;
-    Serial.print("Blue\n");
+    //Serial.print("Blue\n");
 
     //This is in place so that the Arduino will take data when the Pixy is at
     //50 frames per second to prevent the Pixy from overloading
     if (i%50==0)
     {
-
       //this is where we want to write
-
     }
   }
   else
-    
   {
     // return -1
-    Serial.print("The\n");
+    //Serial.print("The\n");
   }
   
 }
