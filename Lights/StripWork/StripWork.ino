02246b80af81e169a727517ddaf349cf8c75eaa0
Seth Frank <21frankse@fpsct.org>
2019-02-09 17:39:50 -0500

    added some patterns from last years code

--- Lights/StripWork/StripWork.ino
+++ Lights/StripWork/StripWork.ino
@@ -12,7 +12,6 @@
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
 String myWord = "";
-boolean isON;
 
 void setup()
 {
@@ -42,14 +41,15 @@ void loop()
   //turnOff();
   //when the roborio sends a message from an input, it will initiate the correct if statement
   String message = myWord;
-  if (message.equals("o"))
+
+  
+  if (message.equals("l"))
   {
     turnRGBFlash(255, 40, 0); //cargo orange; press left bumper
   }
 
-  if (message.equals("f"))
+  if (message.equals("r"))
   {
-    //turnOff();
     turnRGBFlash(175,75,0); //hatch panel yellow; press right bumper
   }
 
@@ -62,7 +62,21 @@ void loop()
   {
     turnRGBBounce(0,0,255);
   }
-  
+
+  if (message.equals("f"))
+  {
+    turnEnforcers();
+  }
+
+  if (message.equals("s"))
+  {
+    turnFirst();
+  }
+
+  if (message.equals("y"))
+  {
+    turnRainbow();
+  }
   //turnRGBFlash(255, 60, 0); //orange
   //turnRGBBounce(255, 60, 0); //orange
   //turnRGBBounce(200,125,0); //yellow 
@@ -71,6 +85,10 @@ void loop()
   //turnRGBBounce(5,40,132); //blue alliance
   //turnFirst();
   //turnDeepSpace();
+  //turnPolar();
+  //turnRainbow();
+  //turnBlueAndGold();
+  //turnEnforcers();
 }
 
 
@@ -81,18 +99,6 @@ void turnOff()
   for (int i = 0; i <= 30; i++) {
     strip.setPixelColor(i, 0, 0, 0);
   }
-  strip.show();
-}
-
-
-void turnRGB(int R, int G, int B)
-{
-  int numbers[3];
-  numbers[3] = new int[3];
-  numbers[0] = R;
-  numbers[1] = G;
-  numbers[2] = B;
-  strip.setPixelColor(0,R,G,B);
   strip.show();
 }
 
@@ -144,7 +150,7 @@ void turnRGBBounce(int R, int G, int B)
 void turnEnforcers()
 {
   // enforcers Colors:
-  for (int i = 36; i > 0; i--) {
+  for (int i = 30; i > 0; i--) {
     for (int x = 0; x <= 30; x += 4) {
       strip.setPixelColor(i + x, 229, 187, 0);
       strip.setPixelColor(i + (x - 1), 229, 187, 0);
@@ -166,7 +172,7 @@ void turnEnforcers()
 void turnFirst() 
 {
   for (int i=30; i > 0; i--){
-    for(int x= 0; x <=36; x+=4){
+    for(int x= 0; x <=30; x+=4){
       strip.setPixelColor(i+x,237,28,36);
       strip.setPixelColor(i+(x-1),0,101,179);
       strip.setPixelColor(i+(x-2),35,31,32);
@@ -186,7 +192,7 @@ void turnFirst()
 void turnDeepSpace()  
 {
   for (int i=30; i > 0; i--){
-    for(int x= 0; x <=36; x+=4){
+    for(int x= 0; x <=30; x+=4){
       strip.setPixelColor(i+x,239,120,34);
       strip.setPixelColor(i+(x-1),148,148,146);
       //strip.setPixelColor(i+(x-2),35,31,32);
@@ -199,5 +205,43 @@ void turnDeepSpace()
     } 
     strip.show();
     delay(100);
+  }
+}
+
+
+void turnPolar() {
+  for (int i=30; i > 0; i--){
+    for(int x= 0; x <=30; x+=3){
+      strip.setPixelColor(i+x,190,0,90);
+      strip.setPixelColor(i+(x-1),60,49,110);
+      strip.setPixelColor(i+(x-2),0,26,110);
+  
+      strip.setPixelColor(i-x,190,0,90);
+      strip.setPixelColor(i-(x+1),60,49,110);
+      strip.setPixelColor(i-(x+2),0,26,110);
+    } 
+    strip.show();
+    delay(100);
+  }
+}
+
+void turnRainbow() {
+    for (int i=36; i > 0; i--){
+      for(int x= 0; x <=36; x+=6){
+        strip.setPixelColor(i+x,255,0,0);
+        strip.setPixelColor(i+(x-1),255,165,0);
+        strip.setPixelColor(i+(x-2),255,255,0);
+        strip.setPixelColor(i+(x-3),0,255,0);
+        strip.setPixelColor(i+(x-4),0,0,255);
+        strip.setPixelColor(i+(x-5),255,0,255);
+        strip.setPixelColor(i-x,255,0,0);
+        strip.setPixelColor(i-(x+1),255,165,0);
+        strip.setPixelColor(i-(x+2),255,255,0);
+        strip.setPixelColor(i-(x+3),0,255,0);
+        strip.setPixelColor(i-(x+4),0,0,255);
+        strip.setPixelColor(i-(x+5),255,0,255);
+      } 
+      strip.show();
+      delay(100);    
   }
 }
