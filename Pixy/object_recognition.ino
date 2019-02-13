bdb4a8eca11565be5c970b1ff8961c49d5cf4415
Yash Sabarad <30813396+YashSabarad@users.noreply.github.com>
2019-02-13 09:54:41 -0500

    Add files via upload
    code redone to work with pixy2

---
+++ Pixy/object_recognition.ino
@@ -0,0 +1,51 @@
+#include <Pixy2.h>
+#include <Wire.h>
+
+Pixy2 pixy;  
+int val;
+
+void setup() 
+{
+  Wire.begin(8);
+  //Wire.onRequest(requestEvent);
+  
+  pixy.init();
+  pixy.setCameraBrightness(16);
+}
+
+void requestEvent()
+{
+  //should use the value set to val right before requestEvent is called
+  Wire.write(val);
+}
+
+void loop() 
+{ 
+  //Pixy seems to overload at fast speeds, implementing a delay
+  delay(20);
+
+  //counter variable
+  int i;
+
+  //get an array of all objects pixy is currently seeing
+  //ccc stands for color connected components
+  pixy.ccc.getBlocks();
+  
+  //will only run if blocks has values
+  if (pixy.ccc.numBlocks)
+  {
+    for (i = 0; i < pixy.ccc.numBlocks; i++)
+    {
+      val = pixy.ccc.blocks[i].m_x;
+      //m_x is the variable to represent x value from 0 to 315
+      Wire.onRequest(requestEvent);
+    }
+  }
+  
+  else
+  {
+    val = -1;
+    Wire.onRequest(requestEvent);
+  }
+  
+}
