6ef5eeb47b04110f4e52af0c1207729f20cfc420
Seth Frank <21frankse@fpsct.org>
2019-02-13 20:00:08 -0500

    changed values to new strip number

--- Lights/StripWork/StripWork.ino
+++ Lights/StripWork/StripWork.ino
@@ -10,7 +10,7 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(59, PIN, NEO_GRB + NEO_KHZ800);
 String myWord = "";
 
 void setup()
@@ -98,7 +98,7 @@ void loop()
 
 void turnOff()
 {
-  for (int i = 0; i <= 30; i++) {
+  for (int i = 0; i <= 59; i++) {
     strip.setPixelColor(i, 0, 0, 0);
   }
   strip.show();
@@ -113,7 +113,7 @@ void turnRGBFlash(int R, int G, int B)
   numbers[0] = R;
   numbers[1] = G;
   numbers[2] = B;
-  for(int i=0; i <= 30; i++)
+  for(int i=0; i <= 59; i++)
     {
       strip.setPixelColor(i,R,G,B);
       strip.show();
@@ -132,14 +132,14 @@ void turnRGBBounce(int R, int G, int B)
   numbers[0] = R;
   numbers[1] = G;
   numbers[2] = B;
-  for(int i=0 ; i <= 30; i++)
+  for(int i=0 ; i <= 59; i++)
   {
     strip.setPixelColor(i,R,G,B);
     strip.show();
     delay(50);
     strip.show();
   }
-  for(int i=30; i >= 0; i--)
+  for(int i=59; i >= 0; i--)
   {
     strip.setPixelColor(i,0,0,0);
     strip.show();
@@ -152,8 +152,8 @@ void turnRGBBounce(int R, int G, int B)
 void turnEnforcers()
 {
   // enforcers Colors:
-  for (int i = 30; i > 0; i--) {
-    for (int x = 0; x <= 30; x += 4) {
+  for (int i = 59; i > 0; i--) {
+    for (int x = 0; x <= 59; x += 4) {
       strip.setPixelColor(i + x, 229, 187, 0);
       strip.setPixelColor(i + (x - 1), 229, 187, 0);
       strip.setPixelColor(i + (x - 2), 7, 16, 79);
@@ -173,8 +173,8 @@ void turnEnforcers()
 
 void turnFirst() 
 {
-  for (int i=30; i > 0; i--){
-    for(int x= 0; x <=30; x+=4){
+  for (int i=59; i > 0; i--){
+    for(int x= 0; x <=59; x+=4){
       strip.setPixelColor(i+x,237,28,36);
       strip.setPixelColor(i+(x-1),0,101,179);
       strip.setPixelColor(i+(x-2),35,31,32);
@@ -193,8 +193,8 @@ void turnFirst()
 
 void turnDeepSpace()  
 {
-  for (int i=30; i > 0; i--){
-    for(int x= 0; x <=30; x+=4){
+  for (int i=59; i > 0; i--){
+    for(int x= 0; x <=59; x+=4){
       strip.setPixelColor(i+x,239,120,34);
       strip.setPixelColor(i+(x-1),148,148,146);
       //strip.setPixelColor(i+(x-2),35,31,32);
@@ -212,8 +212,8 @@ void turnDeepSpace()
 
 
 void turnPolar() {
-  for (int i=30; i > 0; i--){
-    for(int x= 0; x <=30; x+=3){
+  for (int i=59; i > 0; i--){
+    for(int x= 0; x <=59; x+=3){
       strip.setPixelColor(i+x,190,0,90);
       strip.setPixelColor(i+(x-1),60,49,110);
       strip.setPixelColor(i+(x-2),0,26,110);
@@ -228,8 +228,8 @@ void turnPolar() {
 }
 
 void turnRainbow() {
-    for (int i=36; i > 0; i--){
-      for(int x= 0; x <=36; x+=6){
+    for (int i=59; i > 0; i--){
+      for(int x= 0; x <=59; x+=6){
         strip.setPixelColor(i+x,255,0,0);
         strip.setPixelColor(i+(x-1),255,165,0);
         strip.setPixelColor(i+(x-2),255,255,0);
