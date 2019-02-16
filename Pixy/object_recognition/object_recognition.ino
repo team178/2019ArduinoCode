3674ebd0aa459d1006be26e3e39b81ff27ccef32
Unknown <20kweeda@fpsct.org>
2019-02-16 14:17:26 -0500

    Made it so that we can now detect both pixy objects instead of just one

--- Pixy/object_recognition/object_recognition.ino
+++ Pixy/object_recognition/object_recognition.ino
@@ -3,13 +3,13 @@
 
 Pixy2 pixy;  
 int32_t val;
-byte valArray[2];
+byte valArray[4];
 
 void setup() 
 {
   Wire.begin(8);//technically address doesn't need to be here, but i haven't seen it work without it. (put 8 in as input)
   //Wire.onRequest(requestEvent);
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.print("Starting...\n");
   Wire.onRequest(requestEvent);
   pixy.init();
@@ -19,7 +19,7 @@ void setup()
 void requestEvent()//deprecated atm
 {
   //should use the value set to val right before requestEvent is called
-  Wire.write(valArray, 2);
+  Wire.write(valArray, 4);
 }
 
 void sendFirstX()//sends first x value over i2c address 8, done in order to better differentiate values in java
@@ -52,27 +52,34 @@ void loop()
   //pixy.ccc.getBlocks();
   
   //will only run if blocks has values
-  if (pixy.ccc.getBlocks(1) > 0)
+  if (pixy.ccc.getBlocks() > 0)
   {
-    for (i = 0; i < 2/*pixy.ccc.numBlocks*/; i++)
-    {
-      val = pixy.ccc.blocks[i].m_x;
+   // for (i = 0; i < 2/*pixy.ccc.numBlocks*/; i++)
+  //  {
+      val = pixy.ccc.blocks[0].m_x;
+          pixy.ccc.blocks[0].print();
       //m_x is the variable to represent x value from 0 to 315
-      if (i % 2 == 0)
-      {
+   //   if (i % 2 == 0)
+   //   {
         valArray[1] = val & 0xFF;
         valArray[0] = (val >> 8) & 0xFF;
+        Serial.print(valArray[0]);
+        Serial.print(valArray[1]);
         //Wire.onRequest(sendFirstX);
-        Wire.onRequest(requestEvent);
-        Serial.println(val);
-      } else {
-        valArray[1] = val & 0xFF;
-        valArray[0] = (val >> 8) & 0xFF;
+        //Wire.onRequest(requestEvent);
+        Serial.println("val 1 = " + val);
+  //    } else {
+        val = pixy.ccc.blocks[1].m_x;
+        pixy.ccc.blocks[1].print();
+        valArray[3] = val & 0xFF;
+        valArray[2] = (val >> 8) & 0xFF;
+        Serial.print(valArray[2]);
+        Serial.print(valArray[3]);
         //Wire.onRequest(sendSecondX);
-        Wire.onRequest(requestEvent);
-        Serial.println(val);
-      }
-    }
+        //Wire.onRequest(requestEvent);
+        Serial.println("val 2 = " + val);
+  //    }
+ //   }
   }
   
   else
