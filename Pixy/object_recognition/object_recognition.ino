02acb0c07fa137ac1780ac76960a73b4399e2ffe
Darren Kwee <20kweeda@fpsct.org>
2019-02-16 17:38:10 -0500

    got rid of code for sending across two different buses
    added some comments and removed the for loop(cause it wasn't working for some reason)

--- Pixy/object_recognition/object_recognition.ino
+++ Pixy/object_recognition/object_recognition.ino
@@ -2,115 +2,60 @@
 #include <Wire.h>
 
 Pixy2 pixy;  
-int32_t val;
-byte valArray[4];
+int32_t val;//used to take data from pixy
+byte valArray[4];//to send to roboRIO
 
 void setup() 
 {
-  Wire.begin(8);//technically address doesn't need to be here, but i haven't seen it work without it. (put 8 in as input)
-  //Wire.onRequest(requestEvent);
+  Wire.begin(8);//should correspond to RobotMap.pixyAddress
   Serial.begin(115200);
   Serial.println("Starting...");
-  Wire.onRequest(requestEvent);
+  Wire.onRequest(requestEvent);//is essentially a definition, what to do when the roboRIO asks for data
   pixy.init();
   pixy.setCameraBrightness(16);
 }
 
-void requestEvent()//deprecated atm
+void requestEvent()//handler for onRequest method
 {
-  //should use the value set to val right before requestEvent is called
-  Wire.write(valArray, 4);
+  Wire.write(valArray, 4);//sending four bytes, two bytes for each 
 }
-
-void sendFirstX()//sends first x value over i2c address 8, done in order to better differentiate values in java
-{
-  Wire.begin(8);
-  //Wire.beginTransmission(8);
-  Wire.write(valArray, 2);
-  //Wire.endTransmission(8);
-}
-
-void sendSecondX()//sends second x value over i2c address 9, see above
-{
-  Wire.begin(9);
-  //Wire.beginTransmission(9);
-  Wire.write(valArray, 2);
-  //Wire.endTransmission(9);
-}
-
 
 void loop() 
 { 
   //Pixy seems to overload at fast speeds, implementing a delay
   delay(20);
 
-  //counter variable
-  int i;
-
   //get an array of all objects pixy is currently seeing
   //ccc stands for color connected components
-  //pixy.ccc.getBlocks();
-  
   //will only run if blocks has values
-  if (pixy.ccc.getBlocks() > 0)
+  if (pixy.ccc.getBlocks() > 0)//updates the blocks[] with the current objects the pixy, returning if it worked (value higher than 0)
   {
-   // for (i = 0; i < 2/*pixy.ccc.numBlocks*/; i++)
-  //  {
-      val = pixy.ccc.blocks[0].m_x;
-          pixy.ccc.blocks[0].print();
-      //m_x is the variable to represent x value from 0 to 315
-   //   if (i % 2 == 0)
-   //   {
-        valArray[1] = val & 0xFF;
+        //sends first x value 
+        val = pixy.ccc.blocks[0].m_x;//takes x value from 0th position in array from getBlocks();
+        pixy.ccc.blocks[0].print();//prints data about the block, like it literally prints all of the info stored
+        valArray[1] = val & 0xFF;//shifts byte over, then bitwise and, essentially to be able to send the values
         valArray[0] = (val >> 8) & 0xFF;
         Serial.print(valArray[0]);
         Serial.print(valArray[1]);
-        //Wire.onRequest(sendFirstX);
-        //Wire.onRequest(requestEvent);
+        Serial.print(" is first x value");
         Serial.println("val 1 = " + val);
-  //    } else {
+
+        //sends second x value 
         val = pixy.ccc.blocks[1].m_x;
-        pixy.ccc.blocks[1].print();
-        valArray[3] = val & 0xFF;
+        pixy.ccc.blocks[1].print();//see above
+        valArray[3] = val & 0xFF;//same as above
         valArray[2] = (val >> 8) & 0xFF;
         Serial.print(valArray[2]);
         Serial.print(valArray[3]);
-        //Wire.onRequest(sendSecondX);
-        //Wire.onRequest(requestEvent);
+        Serial.print(" is second x value");
         Serial.println("val 2 = " + val);
-  //    }
- //   }
   }
   
   else
   {
-    val = 0;
-    valArray[1] = val & 0xFF;
+    val = 0;//if no objects are recongnized, sends zero 
+    valArray[1] = val & 0xFF;//same as above 
     valArray[0] = (val >> 8) & 0xFF;
-
-    for (i = 0; i < 2; i++) {
-      Serial.println((byte) valArray[i]);
-      }
-    
-    //will return value of -1 when nothing is recognized
-   /* val = 0;
-    while (val < 320) {
-    Serial.println(val);//pnly one works for some reason 
-    val = val + 1;//pnly one works for some reason
-    //Wire.onRequest(sendFirstX);
-    delay(1000);
-    } */
-
-    /*
-    while (val >= 0) {
-      Serial.println(val);
-      val--;
-      Wire.onRequest(sendSecondX);
-      delay(1000);
-    }
-    
-    //Wire.onRequest(requestEvent);
-    */
   }
   
 }
