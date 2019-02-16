277b6218a0529608c91206432189829bde0d9456
Unknown <20kweeda@fpsct.org>
2019-02-16 12:50:43 -0500

    Got sending pixy values from one I2C bus working

--- Pixy/object_recognition/object_recognition.ino
+++ Pixy/object_recognition/object_recognition.ino
@@ -2,15 +2,16 @@
 #include <Wire.h>
 
 Pixy2 pixy;  
-int16_t val;
+int32_t val;
+byte valArray[2];
 
 void setup() 
 {
   Wire.begin(8);//technically address doesn't need to be here, but i haven't seen it work without it. (put 8 in as input)
-  Wire.onRequest(requestEvent);
+  //Wire.onRequest(requestEvent);
   Serial.begin(9600);
   Serial.print("Starting...\n");
-  //Wire.onRequest(requestEvent);
+  Wire.onRequest(requestEvent);
   pixy.init();
   pixy.setCameraBrightness(16);
 }
@@ -18,23 +19,23 @@ void setup()
 void requestEvent()//deprecated atm
 {
   //should use the value set to val right before requestEvent is called
-  Wire.write(val);
+  Wire.write(valArray, 2);
 }
 
 void sendFirstX()//sends first x value over i2c address 8, done in order to better differentiate values in java
 {
   Wire.begin(8);
-  Wire.beginTransmission(8);
-  Wire.write(val);
-  Wire.endTransmission(8);
+  //Wire.beginTransmission(8);
+  Wire.write(valArray, 2);
+  //Wire.endTransmission(8);
 }
 
 void sendSecondX()//sends second x value over i2c address 9, see above
 {
   Wire.begin(9);
-  Wire.beginTransmission(9);
-  Wire.write(val);
-  Wire.endTransmission(9);
+  //Wire.beginTransmission(9);
+  Wire.write(valArray, 2);
+  //Wire.endTransmission(9);
 }
 
 
@@ -48,21 +49,27 @@ void loop()
 
   //get an array of all objects pixy is currently seeing
   //ccc stands for color connected components
-  pixy.ccc.getBlocks();
+  //pixy.ccc.getBlocks();
   
   //will only run if blocks has values
-  if (pixy.ccc.numBlocks)
+  if (pixy.ccc.getBlocks(1) > 0)
   {
-    for (i = 0; i < pixy.ccc.numBlocks; i++)
+    for (i = 0; i < 2/*pixy.ccc.numBlocks*/; i++)
     {
       val = pixy.ccc.blocks[i].m_x;
       //m_x is the variable to represent x value from 0 to 315
       if (i % 2 == 0)
       {
-        Wire.onRequest(sendFirstX);
+        valArray[1] = val & 0xFF;
+        valArray[0] = (val >> 8) & 0xFF;
+        //Wire.onRequest(sendFirstX);
+        Wire.onRequest(requestEvent);
         Serial.println(val);
       } else {
-        Wire.onRequest(sendSecondX);
+        valArray[1] = val & 0xFF;
+        valArray[0] = (val >> 8) & 0xFF;
+        //Wire.onRequest(sendSecondX);
+        Wire.onRequest(requestEvent);
         Serial.println(val);
       }
     }
@@ -70,13 +77,33 @@ void loop()
   
   else
   {
+    val = 0;
+    valArray[1] = val & 0xFF;
+    valArray[0] = (val >> 8) & 0xFF;
+
+    for (i = 0; i < 2; i++) {
+      Serial.println((byte) valArray[i]);
+      }
+    
     //will return value of -1 when nothing is recognized
+   /* val = 0;
     while (val < 320) {
     Serial.println(val);//pnly one works for some reason 
     val = val + 1;//pnly one works for some reason
+    //Wire.onRequest(sendFirstX);
     delay(1000);
+    } */
+
+    /*
+    while (val >= 0) {
+      Serial.println(val);
+      val--;
+      Wire.onRequest(sendSecondX);
+      delay(1000);
     }
+    
     //Wire.onRequest(requestEvent);
+    */
   }
   
 }
