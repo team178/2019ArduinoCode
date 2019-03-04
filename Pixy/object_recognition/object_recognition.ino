c5584ccdf0cf0df16f5d1c16b5af130b56cf103c
Unknown <20kweeda@fpsct.org>
2019-03-04 17:05:00 -0500

    reorganized loops to be more efficient, commented out print statements

--- Pixy/object_recognition/object_recognition.ino
+++ Pixy/object_recognition/object_recognition.ino
@@ -29,43 +29,39 @@ void loop()
   //ccc stands for color connected components
   //will only run if blocks has values
   
-  if (pixy.ccc.getBlocks() == 0 || pixy.ccc.getBlocks == 1)//when not enough objects are recongnized 
+  if (pixy.ccc.getBlocks() == 0 || pixy.ccc.getBlocks() == 1)//when not enough objects are recongnized 
   {
     val = 316;//to be sent when no objects are detected 
     valArray[1] = val & 0xFF;//same as others 
     valArray[0] = (val >> 8) & 0xFF;
-  }
-
-  if (pixy.ccc.getBlocks() == 3)//when three objects are detected
+  } else if (pixy.ccc.getBlocks() == 3)//when three objects are detected
   {
     val = 317;//to be sent when three objects are detected 
     valArray[1] = val & 0xFF;//same as others 
     valArray[0] = (val >> 8) & 0xFF;
-  }
-    
-  if (pixy.ccc.getBlocks() > 0 && pixy.ccc.getBlocks() =< 3)//updates the blocks[] with the current objects the pixy, returning if it worked (value higher than 0)
+  } else if (pixy.ccc.getBlocks() == 2)//updates the blocks[] with the current objects the pixy, returning if it worked (value higher than 0)
   {
-        //sends first x value 
+        //sends first x value
         val = pixy.ccc.blocks[0].m_x;//takes x value from 0th position in array from getBlocks();
-        pixy.ccc.blocks[0].print();//prints data about the block, like it literally prints all of the info 
+        //pixy.ccc.blocks[0].print();//prints data about the block, like it literally prints all of the info 
         delay(100);
         valArray[1] = val & 0xFF;//shifts byte over, then bitwise and, essentially to be able to send the values
         valArray[0] = (val >> 8) & 0xFF;
-        Serial.print(valArray[0]);
+        /*Serial.print(valArray[0]);
         Serial.print(valArray[1]);
         Serial.print(" is first x value");
-        Serial.println("val 1 = " + val);
+        Serial.println("val 1 = " + val);*/
 
         //sends second x value 
         val = pixy.ccc.blocks[1].m_x;
-        pixy.ccc.blocks[1].print();//see above
+        ///pixy.ccc.blocks[1].print();//see above
         delay(100);
         valArray[3] = val & 0xFF;//same as above
         valArray[2] = (val >> 8) & 0xFF;
-        Serial.print(valArray[2]);
+        /*Serial.print(valArray[2]);
         Serial.print(valArray[3]);
         Serial.print(" is second x value");
-        Serial.println("val 2 = " + val);
+        Serial.println("val 2 = " + val);*/
   }
 
   /*//old soln, commented out for now to test
